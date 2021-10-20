d3054685dd947bf65bbafee3425d8744a4b96740
k1oo <kbaek1202@gmail.com>
2021-10-21 00:03:58 +0900

    Set NestJS working environment

--- src/hello/HelloController.ts
+++ src/hello/HelloController.ts
@@ -4,7 +4,7 @@ import { Controller, Get } from '@nestjs/common';
 export class HelloController {
   constructor() {}
 
-  @Get('/hello')
+  @Get('/')
   async hello() {
     return 'Hello, world';
   }
